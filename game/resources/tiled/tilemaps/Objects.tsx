d6bd4018f3ccee725f1ff93de926cc63fa6a626d
Onno <onnokh@hotmail.com>
2025-10-10 22:26:42 +0200

    add island

---
+++ game/resources/tiled/tilemaps/Objects.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Objects" tilewidth="96" tileheight="96" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <tile id="1">
+  <image source="../../reactor/reactor-Sheet.png" width="96" height="96"/>
+ </tile>
+ <tile id="2">
+  <image source="../../objects/tree.png" width="48" height="96"/>
+ </tile>
+</tileset>
